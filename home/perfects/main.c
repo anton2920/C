e663b5065a839cf74dce17adf904da0b915259fb
anton2920 <anton2920@gmail.com>
2022-10-02 16:41:40 +0300

    PC: drastically improved performance of perfect numbers calculator

--- home/perfects/main.c
+++ home/perfects/main.c
@@ -10,9 +10,6 @@
 #define P_MAX_BOUND 82589932
 
 
-static bool exitted = false;
-
-
 mpz_ptr get_mersenne(unsigned long p)
 {
     mpz_ptr mersenne;
@@ -51,10 +48,11 @@ mpz_ptr get_perfect(unsigned long p)
 }
 
 
-bool is_prime(mpz_ptr num)
+bool is_definitely_prime(mpz_ptr num)
 {
-    mpz_t bound, i, tmp;
     bool result = true;
+    mpz_t bound, i;
+    int first_bit;
 
     assert(num != NULL);
 
@@ -63,38 +61,52 @@ bool is_prime(mpz_ptr num)
     } else if (mpz_cmp_ui(num, 2) == 0) {
         return true;
     } else {
-        mpz_init(tmp);
-        mpz_mod_ui(tmp, num, 2);
-        if (mpz_cmp_ui(tmp, 0) == 0) {
-            mpz_clear(tmp);
+        first_bit = mpz_tstbit(num, 0);
+        if (first_bit == 0) {
             return false;
         }
-        mpz_clear(tmp);
     }
 
-    mpz_init(tmp);
     mpz_init(bound);
 
     mpz_sqrt(bound, num);
-    for (mpz_init_set_ui(i, 3); (mpz_cmp(i, bound) <= 0) && (!exitted); mpz_add_ui(i, i, 2)) {
-        mpz_mod(tmp, num, i);
-        if (mpz_cmp_ui(tmp, 0) == 0) {
+    for (mpz_init_set_ui(i, 3); mpz_cmp(i, bound) <= 0; mpz_add_ui(i, i, 2)) {
+        if (mpz_divisible_p(num, i)) {
             result = false;
             break;
         }
     }
 
     mpz_clear(bound);
-    mpz_clear(tmp);
     mpz_clear(i);
 
-    return (!exitted) ? result : false;
+    return result;
+}
+
+
+bool is_prime(mpz_ptr num)
+{
+    assert(num != NULL);
+
+    /* Use probabilistic test first */
+    switch (mpz_probab_prime_p(num, 25)) {
+        case 2:
+            return true;
+        case 1:
+            return true;
+            /* Algorithm is not sure about this number, check the old way */
+            /*is_definitely_prime(num);*/
+        case 0:
+            return false;
+        default:
+            break;
+    }
 }
 
 
 void sighandler(int signo)
 {
-    exitted = true;
+    exit(0);
 }
 
 
@@ -108,7 +120,7 @@ int main()
 
     gettimeofday(&start_time, NULL);
 
-#pragma omp parallel for num_threads(4), schedule(static, 1), default(none), shared(exitted, start_time)
+#pragma omp parallel for num_threads(8), schedule(static, 1), default(none), shared(start_time)
     for (p = 2; p < P_MAX_BOUND; ++p) {
         mpz_ptr num = get_mersenne(p);
         assert(num != NULL);
@@ -120,19 +132,17 @@ int main()
 
             gettimeofday(&end_time, NULL);
 #pragma omp critical
-            gmp_printf("[TID=%d][P=%lu][PN=%Zd] Time is %lf\n", omp_get_thread_num(), p, perfect_number,
-                       (end_time.tv_sec + (double) end_time.tv_usec / 1000000) -
-                       (start_time.tv_sec + (double) start_time.tv_usec / 1000000));
+            {
+                gmp_printf("[TID=%d][P=%lu][PN=%Zd] Time is %lf\n", omp_get_thread_num(), p, perfect_number,
+                           (end_time.tv_sec + (double) end_time.tv_usec / 1000000) -
+                           (start_time.tv_sec + (double) start_time.tv_usec / 1000000));
+            }
 
             mpz_clear(perfect_number);
             free(perfect_number);
         }
         mpz_clear(num);
         free(num);
-
-        if (exitted) {
-            p = P_MAX_BOUND;
-        }
     }
 
     return 0;
